ced85bd134a2b9b4b471a2f6b622d86b8616fa45
Celia Mateos Martin <cmateos-@c1r10s3.42madrid.com>
2023-07-31 20:18:04 +0200

    falta normi, todo ok sin leaks

--- src/main.c
+++ src/main.c
@@ -10,115 +10,107 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "../push_swap.h"
-void    ft_leaks(void)
+
+void	ft_leaks(void)
 {
-    system("leaks -q push_swap2");
+	system("leaks -q push_swap");
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    //atexit(ft_leaks);
-    t_stack     **stack_a;
-    t_stack     **stack_b;
-    int         stack_size;
-    int i;
+	atexit(ft_leaks);
+	t_stack	**stack_a;
+	t_stack	**stack_b;
+	int		stack_size;
+	int		i;
 
-    i = 1;
-    if (argc < 3)
-    {
-        printf("\nError, min 3 argc");
-        exit (1);
-    }
-    stack_a = (t_stack **)malloc(sizeof(t_stack));
-    if (!stack_a)
-        return (0);
-    *stack_a = NULL;
-    while (i != argc)
-    {
-        ft_fill_stack(argv[i], stack_a);
-        i++;
-    }
-
-    stack_size = ft_stacksize(*stack_a);
-    if (!stack_size)
-        return (0);
-    //printf("\nStack_a tiene %i nodos", stack_size);
-    ft_check_arg(stack_a);
-    stack_b = (t_stack **)malloc(sizeof(t_stack));
-    if (!stack_b)
-        ft_error1(stack_a, 0);
-    *stack_b = NULL;
-    
-    ft_print_stack(*stack_a, *stack_b);
-    ft_index(stack_a, stack_size);
-    push_swap(stack_a, stack_b);
-    printf("\n\n\n\n DESPUÉS DE PUSH_SWAP:");
-    ft_print_stack(*stack_a, *stack_b);
-    exit (1);
-    return (0);
+	i = 1;
+	stack_a = (t_stack **)malloc(sizeof(t_stack));
+	if (!stack_a)
+		return (0);
+	*stack_a = NULL;
+	while (i != argc)
+	{
+		ft_fill_stack(argv[i], stack_a);
+		i++;
+	}
+	stack_size = ft_stacksize(*stack_a);
+	if (stack_size < 2)
+		ft_error1(stack_a, 0);
+	//printf("\nStack_a tiene %i nodos\n", stack_size);
+	ft_check_arg(stack_a);
+	stack_b = (t_stack **)malloc(sizeof(t_stack));
+	if (!stack_b)
+		ft_error1(stack_a, 0);
+	*stack_b = NULL;
+	//ft_print_stack(*stack_a, *stack_b);
+	ft_index(stack_a, stack_size);
+	push_swap(stack_a, stack_b);
+	//printf("\n\n\n\n DESPUÉS DE PUSH_SWAP:");
+	//ft_print_stack(*stack_a, *stack_b);
+	ft_stackclear(stack_a);
+	ft_stackclear(stack_b);
+	exit (0);
+	return (0);
 }
 
-
-
-
-
-void    ft_print_stack(t_stack *stack_a, t_stack *stack_b)
+void	ft_print_stack(t_stack *stack_a, t_stack *stack_b)
 {
-    if (stack_a == NULL)
-        printf("\n\nStack_a is null");
-    if (stack_b == NULL)
-        printf("\n\nStack_b is null");
-    while (stack_a)
-    {
-       printf("\nSTACK_A VALUE:%ld", stack_a->value);
-       printf("\nSTACK_A      index:%d", stack_a->index);
-       stack_a = stack_a->next;
-    }
-    printf("\n\n\n");
-    while (stack_b)
-    {
-       printf("\n\nSTACK_B:%ld", stack_b->value);
-       stack_b = stack_b->next;
-    }
+	if (stack_a == NULL)
+		printf("\n\nStack_a is null");
+	if (stack_b == NULL)
+		printf("\n\nStack_b is null");
+	while (stack_a)
+	{
+	   printf("\nSTACK_A VALUE:%d", stack_a->value);
+	   printf("\nSTACK_A      index:%d", stack_a->index);
+	   stack_a = stack_a->next;
+	}
+	printf("\n\n\n");
+	while (stack_b)
+	{
+	   printf("\n\nSTACK_B:%d", stack_b->value);
+	   stack_b = stack_b->next;
+	}
 }
 /*int main (void)
 {
-    t_list  *stack_a;
-    t_list  *stack_b;
-    t_list *nodo1;
-    t_list *nodo2;
+	t_list  *stack_a;
+	t_list  *stack_b;
+	t_list *nodo1;
+	t_list *nodo2;
 
-    stack_a = NULL;
-    stack_b = NULL;
+	stack_a = NULL;
+	stack_b = NULL;
 
-    nodo1 = ft_lstnew("nodo1");
-    nodo2 = ft_lstnew("nodo2");
-    ft_lstadd_back( &stack_a, nodo1);
-    ft_lstadd_back( &stack_a, nodo2);
+	nodo1 = ft_lstnew("nodo1");
+	nodo2 = ft_lstnew("nodo2");
+	ft_lstadd_back( &stack_a, nodo1);
+	ft_lstadd_back( &stack_a, nodo2);
 
-    while (stack_a)
-    {
-       printf("\nstack_a:%s", stack_a->content);
-       stack_a = stack_a->next;
-    }
+	while (stack_a)
+	{
+	   printf("\nstack_a:%s", stack_a->content);
+	   stack_a = stack_a->next;
+	}
 
-    //push_b(&stack_a, &stack_b, nodo1);
+	//push_b(&stack_a, &stack_b, nodo1);
 
-    stack_b = ft_lstnew(nodo1);
-    ft_lstadd_front(&stack_b, nodo1);
-    //ft_lstdelone(nodo1, free);
+	stack_b = ft_lstnew(nodo1);
+	ft_lstadd_front(&stack_b, nodo1);
+	//ft_lstdelone(nodo1, free);
 
-    while (stack_b)
-    {
-       printf("\nstack_b:%s", stack_b->content);
-       stack_b = stack_b->next;
-    }
+	while (stack_b)
+	{
+	   printf("\nstack_b:%s", stack_b->content);
+	   stack_b = stack_b->next;
+	}
 
-    while (stack_a)
-    {
-       printf("\nstack_a:%s", stack_a->content);
-       stack_a = stack_a->next;
-    }
+	while (stack_a)
+	{
+	   printf("\nstack_a:%s", stack_a->content);
+	   stack_a = stack_a->next;
+	}
 
-    return (0);
+	return (0);
 }*/
